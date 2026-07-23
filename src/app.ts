97f5b8376c649cc6a79915fd38442621df7e6ee4
Rocky Chowdhury <rocky20809@gmail.com>
2026-07-23 22:58:29 +0600

    refine codebase

--- src/app.ts
+++ src/app.ts
@@ -32,9 +32,7 @@ app.use(cors(
     }
 ));
 
-app.post("/api/subscription/webhook",express.raw({type: 'application/json'}),()=>{
-    
-});
+app.use("/api/payments/webhook",express.raw({ type: 'application/json' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
